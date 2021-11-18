35dffe9ca2d91a08d0b2d1f46928d4a127b2f78d
Jordan-Negreiros <jordannegreirossantos@gmail.com>
2021-11-17 21:46:05 -0300

    Cap 2-2

--- cap2-1/main.go
+++ cap2-1/main.go
@@ -3,6 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	numerodebytes, erros := fmt.Println("Hello World", "Hello", 100)
-	fmt.Println(numerodebytes, erros)
+	// numerodebytes, erros := fmt.Println("Hello World", "Hello", 100)
+	_, erros := fmt.Println("Hello World", "Hello", 100) // _ -> ignora a variável
+	fmt.Println(erros)
+
+	x := 16
+	y := "string"
+	z := true
+
+	fmt.Println(x, y, z)
 }
