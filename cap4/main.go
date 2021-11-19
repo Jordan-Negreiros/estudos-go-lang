81b053c35c68958db15c0a35557401637bfd5d0b
Jordan-Negreiros <jordannegreirossantos@gmail.com>
2021-11-19 17:13:34 -0300

    nested loop

--- cap4/main.go
+++ cap4/main.go
@@ -3,7 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	for x := 0; x < 10; x++ {
-		fmt.Printf("%v ", x)
+	for horas := 0; horas <= 12; horas++ {
+
+		fmt.Printf("horas: %v \n", horas)
+
+		for minutos := 0; minutos <= 60; minutos++ {
+			if minutos < 10 {
+				fmt.Printf("%v:0%v  ", horas, minutos)
+			} else {
+				fmt.Printf("%v:%v  ", horas, minutos)
+			}
+		}
+		fmt.Printf("\n")
 	}
 }
