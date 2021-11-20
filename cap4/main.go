0834873f9dcc01ae35843d4886825bbeb6dcee26
Jordan-Negreiros <jordannegreirossantos@gmail.com>
2021-11-20 18:50:17 -0300

    declaração if

--- cap4/main.go
+++ cap4/main.go
@@ -5,15 +5,22 @@ import "fmt"
 func main() {
 	for horas := 0; horas <= 12; horas++ {
 
-		fmt.Printf("horas: %v \n", horas)
+		if horas < 10 {
+			fmt.Printf("horas: 0%v \n", horas)
+		} else {
+			fmt.Printf("horas: %v \n", horas)
+		}
 
 		for minutos := 0; minutos <= 60; minutos++ {
+
 			if minutos < 10 {
 				fmt.Printf("%v:0%v  ", horas, minutos)
 			} else {
 				fmt.Printf("%v:%v  ", horas, minutos)
 			}
+
 		}
+
 		fmt.Printf("\n")
 	}
 }
