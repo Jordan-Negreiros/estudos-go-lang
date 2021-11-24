e94d181cf49139969f6f8354613338d4d7f7b5d7
Jordan-Negreiros <jordannegreirossantos@gmail.com>
2021-11-23 22:27:40 -0300

    funções e Interfaces

--- cap12/funcoes/main.go
+++ cap12/funcoes/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import "fmt"
 
+type Empregado interface {
+	imprimirNome(nome string)
+	imprimirSalario(base int, taxa int) int
+}
+
+type Emp int
+
+func (e Emp) imprimirNome(nome string) {
+	fmt.Println("Empregado Id: \t", e)
+	fmt.Println("Empregado Nome: \t", nome)
+}
+
+func (e Emp) imprimirSalario(base int, taxa int) int {
+	var salario = (base * taxa) / 100
+	return base - salario
+}
+
+func main() {
+	var e1 Empregado
+	e1 = Emp(1)
+	e1.imprimirNome("Nome")
+	fmt.Println("Salário:", e1.imprimirSalario(20000, 5))
 }
